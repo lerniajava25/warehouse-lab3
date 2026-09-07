60893b62c2d0c7afeab18bb057199d010265a810
Paul Ibrahim <paul.ibrahim@hotmail.se>
2026-09-07 12:01:46 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/warehouse/WarehouseLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseLab3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
